469d874ea5d9643145aba7d342852fc80c1424b2
maotora <maotoramakweba@live.com>
2021-04-24 11:56:55 +0300

    Added Countries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { RegionsModule } from './regions/regions.module';
+import { CountriesModule } from './countries/countries.module';
 
 @Module({
-  imports: [RegionsModule],
+  imports: [RegionsModule, CountriesModule],
 })
 export class AppModule {}
